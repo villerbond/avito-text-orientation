747633f38df99e8d966cf1eea705d5c786744568
villerbond <v1llerbond@yandex.ru>
2026-09-20 19:42:54 +0300

    Minor changes

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ def set_seed(seed: int = 42):
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
 def get_device():
